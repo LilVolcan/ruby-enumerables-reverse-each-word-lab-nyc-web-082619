2cf53fd3163d51a87cf55f7e08c5a08957e39511
Erin Volcan <erinvolcan@gmail.com>
2019-08-08 19:39:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,8 +6,8 @@ def reverse_each_word(sentence1)
   return array.join(' ')
 end 
 
-def reverse_each_word(sentence1)
-  array = sentence1.split(/ /)
-  array.collect { |n| n.reverse }
-    return array.join(' ')
-end 
+# def reverse_each_word(sentence1)
+#   array = sentence1.split(/ /)
+#   array.collect { |n| n.reverse }
+#     return array.join(' ')
+# end 
